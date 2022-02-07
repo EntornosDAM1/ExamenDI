b60cc8ef9ea56c1929c102b24bf26ad14ce122af
EntornosDAM1 <hgomezv01@educantabria.es>
2022-02-07 11:42:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-lq7yux
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-lq7yux)
---
+++ src/Arrays/MenuElements.js
@@ -0,0 +1,20 @@
+export const MenuElements=[
+  {
+    id:1,
+    title:'Ejercicio1',
+    path:'./Ejercicio1',
+    component: Ejercicio1
+  },
+  {
+    id:2,
+    title:'Ejercicio2',
+    path:'./Ejercicio2',
+    component: Ejercicio2
+  },
+  {
+    id:3,
+    title:'Ejercicio3',
+    path:'./Ejercicio3',
+    component:Ejercicio3
+  }
+];
